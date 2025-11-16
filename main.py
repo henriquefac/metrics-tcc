33b8249a70184e0ecc9de7fe75e9b61ec036783d
henriquefac <henriquefac@edu.unifor.br>
2025-11-16 00:49:08 -0300

    Adicionar embedding de texto para começar as metricas por similaridade

---
+++ main.py
@@ -0,0 +1,14 @@
+from configPy import EnvManager
+from llama_index.embeddings.ollama import OllamaEmbedding
+
+ollamenv = EnvManager.ollama()
+
+base = f"http://{ollamenv.BASE_OLLAMA}:{ollamenv.PORT_OLLAMA}"
+
+embed_ollama = OllamaEmbedding(ollamenv.EMBEDDING_MODEL, base)
+
+embeddings = embed_ollama.get_text_embedding_batch(
+    ["This is a passage!", "This is another passage"], show_progress=True
+)
+print(f"Got vectors of length {len(embeddings[0])}")
+print(embeddings[0][:10])
