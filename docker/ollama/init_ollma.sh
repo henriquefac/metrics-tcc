c1593d11ccfa0594cda93cb55bc35d0e7fc0b633
henriquefac <henriquefac@edu.unifor.br>
2025-11-17 11:15:38 -0300

    Correção no script de ativação do ollama e script de de teste de similaridade da transcrição apenas com as falas (sem time stamp ou notação de falante)

--- docker/ollama/init_ollma.sh
+++ docker/ollama/init_ollma.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-docker exec -it ollama ollama pull nomic-embed-text
+
+docker run -d \
+    -v ollama:/root/.ollama \
+    -p 11434:11434 \
+    --name ollama \
+    ollama/ollama || true
+
+docker start ollama
+
+docker exec ollama ollama pull nomic-embed-text
+
--- testes/teste_similarity_by_text.py
+++ testes/teste_similarity_by_text.py
@@ -1,6 +1,8 @@
 from configPy import EnvManager, Config
 from transcription_processing.transcricao.findPattern import TranscriptionParser
 from llama_index.embeddings.ollama import OllamaEmbedding
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 
 # diretório dos audios e atas
 output_dir = Config.get_dir_output()
@@ -25,10 +27,30 @@ with open(teste_audio, "r", encoding="utf-8") as audio:
 with open(teste_ata, "r", encoding="utf-8") as ata:
     ata_text = ata.read()
 
+# reconstruir transcrição apenas com o texto das falas
+
+new_text_audio = ""
+parser = TranscriptionParser(audio_text)
+
+for speech in parser.speeches:
+    new_text_audio += speech.text
+    new_text_audio += "\n\n"
+
+
 # configuração do ollama
 ollamenv = EnvManager.ollama()
 base = f"http://{ollamenv.BASE_OLLAMA}:{ollamenv.PORT_OLLAMA}"
 
 embedder = OllamaEmbedding(ollamenv.EMBEDDING_MODEL, base)
 
-transcription = TranscriptionParser(audio_text)
+embeds = embedder.get_text_embedding_batch([new_text_audio, ata_text])
+
+audio_vec = np.array(embeds[0], np.reshape(1, -1))
+ata_vec = np.array(embeds[1], np.reshape(1, -1))
+
+# similaridade do cosseno
+sim = cosine_similarity(audio_vec, ata_vec)[0][0]
+
+print("==================================")
+print(f"Similaridade semântica (coseno): {sim:.4f}")
+print("==================================")
